ce101f23fa693bbaf94e9e26e3716c80a887a49c
woolosh <joshuapmorales@gmail.com>
2021-01-22 09:05:23 -0600

    Song started

---
+++ lib/song.rb
@@ -0,0 +1,40 @@
+require 'pry'
+class Song
+
+        @@count = 0
+        @@artists = []
+        @@genres = []
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@artists << self
+        @@genres << self
+    end
+
+    def self.genres
+        self.count.select {|song| song.genre}
+    end
+
+    def self.artists
+        @@artists
+    end
+
+    def self.genres
+        @@genres
+    end
+
+    def self.artist_count
+        @@artists.count
+    end
+
+    def self.genre_count
+        @genres.count
+    end
+
+    def self.count
+        @@count
+    end
+
+end
